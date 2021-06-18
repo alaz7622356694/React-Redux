5fce8fc65dbd0fec82dfbe4621226052d80676a3
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-18 18:38:50 +0430

    Using action by Dispatching

--- App.js
+++ App.js
@@ -6,9 +6,34 @@ const initialState= {
     counter: 0
 }
 const rootReducer = (state = initialState ,action)=>{
+if(action.type==='INCREMENT')
+return{
+    ...state ,
+    counter:state.counter + 1
+}
+else if (action.type==='ADD')
+{
+    return{
+    ...state ,
+    counter:state.counter + action.value}
+}
 return state
 
 }
 const store=createStore(rootReducer)
 
-console.log(store.getState());
+//console.log(store.getState());
+
+store.dispatch({
+    type: 'INCREMENT' 
+
+
+})
+
+store.dispatch({
+    type: 'ADD' , value:2
+
+
+})
+
+console.log(store.getState())
