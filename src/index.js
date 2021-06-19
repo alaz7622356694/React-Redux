9c2aee1c34f099528761bd11e8550e68f9bda0a2
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-20 00:10:18 +0430

    creating reducer and add it to index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import {createStore} from 'redux'
 
 import App from './App'
-
+import reducer from './store/reducer'
 import './index.css'
 
-const store=createStore()
+const store=createStore(reducer)
 
 ReactDOM.render(<App />, document.getElementById('root'))
