206b191532c7aedd4acffed3b387f3320e920139
alaz7622356694 <alireza.azari2235@outlook.com>
2021-06-20 20:38:14 +0430

    adding redux devtool chrome extension

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App'
 import reducer from './store/reducer'
 import './index.css'
 
-const store=createStore(reducer)
+const store=createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
 
